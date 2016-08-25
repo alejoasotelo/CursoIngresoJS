23a912152eb7e31e10663b01554a21233ac0612f
alejoasotelo <alesotelo@gmail.com>
2016-08-25 12:11:00 -0300

    Ejercicio adivina el numero 1 resuelto.

--- 8-TPs/jsAdivinaElNumero1.js
+++ 8-TPs/jsAdivinaElNumero1.js
@@ -1,26 +1,36 @@
-/*Al comenzar el juego generamos un número 
-secreto del 1 al 100, en la pantalla del juego
- dispondremos de un cuadro de texto para ingresar
-  un número y un botón “Verificar”, si el número 
-  ingresado es el mismo que el número secreto se dará 
-  por terminado el juego con un mensaje similar a este: 
-“Usted es un ganador!!! y en solo X intentos”.
-de no ser igual se debe informar si “falta…”  para llegar al número secreto  o si “se pasó…”  del número secreto.
-*/
-var numeroSecreto; 
-var contadorIntentos;
+var miAplicacion = angular.module('AdivinaElNumero1', []);
 
-function comenzar()
-{
-	//Genero el número RANDOM entre 1 y 100
-	 
-		//alert(numeroSecreto );
-	
+miAplicacion.controller('controladorADB1', function($scope) {
 
-}
+	$scope.juego = {};
 
-function verificar()
-{
-	
-	
-}
+	$scope.comenzar = function(){
+
+		$scope.juego.resp_ingresada = 0;
+		$scope.juego.resp_correcta = Math.floor(Math.random()*100);
+		$scope.juego.intentos = 0;
+
+	}
+
+	$scope.verificar = function(){
+
+		if ($scope.juego.resp_ingresada == $scope.juego.resp_correcta)
+		{
+			alert("Usted es un ganador con " + $scope.juego.intentos);
+			$scope.comenzar();
+		}
+		else
+		{
+			if ($scope.juego.resp_ingresada > $scope.juego.resp_correcta)
+				alert('Te pasaste...');
+			else
+				alert('Te falto...')
+
+			$scope.juego.intentos++;
+
+		}
+	};
+
+	$scope.comenzar();
+
+});
