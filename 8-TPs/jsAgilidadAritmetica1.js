c61bb865a450291672569f6cfc5e8aeac36845fd
alejoasotelo <alesotelo@gmail.com>
2016-08-27 19:29:12 -0300

    Se agularizó y se resolvió el ejercicio AgilidadAritmética 1.

--- 8-TPs/jsAgilidadAritmetica1.js
+++ 8-TPs/jsAgilidadAritmetica1.js
@@ -6,15 +6,66 @@ y una de las cuatro operaciones básicas (suma, resta,
  el resultado de la operación y presionar el botón Aceptar.
 se debe informar si el resultado es el correcto o no.
 */
-var respuesta;
-function comenzar()
-{
+var app = angular.module('AgilidadAritmetica1', []);
 
-	
+app.controller('controladorAA1', function($scope){
 
-}//FIN DE LA FUNCIÓN
-function Responder()
-{
-	
+	var operaciones = ['+', '-', '*', '/'];
 
-}//FIN DE LA FUNCIÓN
+	$scope.juego = {};
+
+	$scope.comenzar = function(){
+
+		$scope.juego.numeros = $scope.generarNumerosYOperador();
+
+		$scope.juego.respuesta_usuario = null;
+
+	}
+
+	$scope.generarNumerosYOperador = function(){
+
+		return {
+			uno: $scope.generarNumeroRandomEntre(1, 10),
+			dos: $scope.generarNumeroRandomEntre(1, 10),
+			operador: operaciones[$scope.generarNumeroRandomEntre(0, 3)]
+		}
+
+	}
+
+	$scope.generarNumeroRandomEntre = function(min, max){
+
+		return Math.floor(Math.random() * (max - (min - 1) ) ) + min;
+
+	}
+
+	$scope.responder = function(){
+
+		var resultado = $scope.calcularResultado();
+
+		if ($scope.juego.respuesta_usuario == resultado)
+			alert('¡Respuesta acertada!');
+		else
+			alert('Respuesta equivocada. El resultado correcto es: ' + resultado);
+
+		$scope.comenzar();
+
+	}
+
+	$scope.calcularResultado = function(){
+
+		var resultado = 0;
+
+		if ($scope.juego.numeros.operador == '+')
+			resultado = ($scope.juego.numeros.uno + $scope.juego.numeros.dos);
+		else if ($scope.juego.numeros.operador == '-')
+			resultado = ($scope.juego.numeros.uno - $scope.juego.numeros.dos);
+		else if ($scope.juego.numeros.operador == '*')
+			resultado = ($scope.juego.numeros.uno * $scope.juego.numeros.dos);
+		else if ($scope.juego.numeros.operador == '/')
+			resultado = ($scope.juego.numeros.uno / $scope.juego.numeros.dos);
+
+		return Math.floor(resultado);
+
+	}
+
+});
